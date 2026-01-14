9b27853850e03db555496b2e1960a77d3649b931
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-01-14 14:47:06 +0530

    update models

--- cspm-api/src/model/entity/entity.py
+++ cspm-api/src/model/entity/entity.py
@@ -1,292 +1,75 @@
-import datetime
 from typing import Optional
+import datetime
 
-from sqlalchemy import (
-    BigInteger,
-    CheckConstraint,
-    Column,
-    DateTime,
-    ForeignKeyConstraint,
-    Integer,
-    PrimaryKeyConstraint,
-    String,
-    Text,
-    UniqueConstraint,
-    text,
-)
+from sqlalchemy import BigInteger, CheckConstraint, Column, DateTime, Enum, ForeignKeyConstraint, JSON, PrimaryKeyConstraint, String, UniqueConstraint, text
 from sqlmodel import Field, Relationship, SQLModel
 
-
-class Actor(SQLModel, table=True):
+class Cloud(SQLModel, table=True):
     __table_args__ = (
-        CheckConstraint("name::text <> ''::text", name="actor_name_check"),
-        PrimaryKeyConstraint("id", name="actor_primary_key"),
-        UniqueConstraint("name", name="actor_unique_name"),
+        CheckConstraint("name::text <> ''::text", name='cloud_name_check'),
+        PrimaryKeyConstraint('id', name='cloud_primary_key'),
+        UniqueConstraint('name', name='cloud_unique_name')
     )
 
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    name: str = Field(sa_column=Column("name", String(200), nullable=False))
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
+    id: int = Field(sa_column=Column('id', BigInteger, primary_key=True))
+    name: str = Field(sa_column=Column('name', String(200), nullable=False))
+    created_at: datetime.datetime = Field(sa_column=Column('created_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    updated_at: datetime.datetime = Field(sa_column=Column('updated_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
 
-    movie_to_actor: list["MovieToActor"] = Relationship(back_populates="actor")
+    credential: list['Credential'] = Relationship(back_populates='cloud')
+    resource: list['Resource'] = Relationship(back_populates='cloud')
 
 
-class Director(SQLModel, table=True):
+class Credential(SQLModel, table=True):
     __table_args__ = (
-        CheckConstraint("name::text <> ''::text", name="director_name_check"),
-        PrimaryKeyConstraint("id", name="director_primary_key"),
-        UniqueConstraint("name", name="director_unique_name"),
+        CheckConstraint("name::text <> ''::text", name='credential_name_check'),
+        ForeignKeyConstraint(['cloud_id'], ['cloud.id'], ondelete='CASCADE', onupdate='CASCADE', name='credential_foreign_key_cloud_id'),
+        PrimaryKeyConstraint('id', name='credential_primary_key'),
+        UniqueConstraint('name', name='credential_unique_name')
     )
 
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    name: str = Field(sa_column=Column("name", String(200), nullable=False))
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
+    id: int = Field(sa_column=Column('id', BigInteger, primary_key=True))
+    name: str = Field(sa_column=Column('name', String(1000), nullable=False))
+    cloud_id: int = Field(sa_column=Column('cloud_id', BigInteger, nullable=False))
+    created_at: datetime.datetime = Field(sa_column=Column('created_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    updated_at: datetime.datetime = Field(sa_column=Column('updated_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
 
-    movie: list["Movie"] = Relationship(back_populates="director")
+    cloud: Optional['Cloud'] = Relationship(back_populates='credential')
+    analysis: list['Analysis'] = Relationship(back_populates='credential')
 
 
-class Genre(SQLModel, table=True):
+class Resource(SQLModel, table=True):
     __table_args__ = (
-        CheckConstraint("name::text <> ''::text", name="genre_name_check"),
-        PrimaryKeyConstraint("id", name="genre_primary_key"),
-        UniqueConstraint("name", name="genre_unique_name"),
+        ForeignKeyConstraint(['cloud_id'], ['cloud.id'], ondelete='CASCADE', onupdate='CASCADE', name='resource_foreign_key_cloud_id'),
+        PrimaryKeyConstraint('id', name='resource_primary_key')
     )
 
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    name: str = Field(sa_column=Column("name", String(200), nullable=False))
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
+    id: int = Field(sa_column=Column('id', BigInteger, primary_key=True))
+    type: str = Field(sa_column=Column('type', String(200), nullable=False, server_default=text("''::character varying")))
+    details: dict = Field(sa_column=Column('details', JSON, nullable=False, server_default=text("'{}'::json")))
+    cloud_id: int = Field(sa_column=Column('cloud_id', BigInteger, nullable=False))
+    created_at: datetime.datetime = Field(sa_column=Column('created_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    updated_at: datetime.datetime = Field(sa_column=Column('updated_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    external_resource_id: Optional[str] = Field(default=None, sa_column=Column('external_resource_id', String(2000)))
 
-    movie_to_genre: list["MovieToGenre"] = Relationship(back_populates="genre")
+    cloud: Optional['Cloud'] = Relationship(back_populates='resource')
+    analysis: list['Analysis'] = Relationship(back_populates='resource')
 
 
-class Movie(SQLModel, table=True):
+class Analysis(SQLModel, table=True):
     __table_args__ = (
-        CheckConstraint("title::text <> ''::text", name="movie_title_check"),
-        ForeignKeyConstraint(
-            ["director_id"],
-            ["director.id"],
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            name="movie_foreign_key_director_id",
-        ),
-        PrimaryKeyConstraint("id", name="movie_primary_key"),
-        UniqueConstraint("title", name="movie_unique_title"),
+        ForeignKeyConstraint(['credential_id'], ['credential.id'], ondelete='CASCADE', onupdate='CASCADE', name='analysis_foreign_key_credential_id'),
+        ForeignKeyConstraint(['resource_id'], ['resource.id'], ondelete='CASCADE', onupdate='CASCADE', name='analysis_foreign_key_resource_id'),
+        PrimaryKeyConstraint('id', name='analysis_primary_key')
     )
 
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    title: str = Field(sa_column=Column("title", String(1000), nullable=False))
-    release_year: int = Field(sa_column=Column("release_year", Integer, nullable=False))
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    director_id: Optional[int] = Field(
-        default=None, sa_column=Column("director_id", BigInteger)
-    )
+    id: int = Field(sa_column=Column('id', BigInteger, primary_key=True))
+    credential_id: int = Field(sa_column=Column('credential_id', BigInteger, nullable=False))
+    resource_id: int = Field(sa_column=Column('resource_id', BigInteger, nullable=False))
+    created_at: datetime.datetime = Field(sa_column=Column('created_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    updated_at: datetime.datetime = Field(sa_column=Column('updated_at', DateTime(True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    current_resource_status: Optional[str] = Field(default=None, sa_column=Column('current_resource_status', Enum('running', 'stopped', name='resource_status')))
+    current_resource_risk: Optional[str] = Field(default=None, sa_column=Column('current_resource_risk', Enum('low', 'high', name='resource_risk')))
 
-    director: Optional["Director"] = Relationship(back_populates="movie")
-    movie_rating: list["MovieRating"] = Relationship(back_populates="movie")
-    movie_to_actor: list["MovieToActor"] = Relationship(back_populates="movie")
-    movie_to_genre: list["MovieToGenre"] = Relationship(back_populates="movie")
-
-
-class MovieRating(SQLModel, table=True):
-    __tablename__ = "movie_rating"
-    __table_args__ = (
-        ForeignKeyConstraint(
-            ["movie_id"],
-            ["movie.id"],
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            name="movie_rating_foreign_key_movie_id",
-        ),
-        PrimaryKeyConstraint("id", name="movie_rating_primary_key"),
-    )
-
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    comment: str = Field(
-        sa_column=Column(
-            "comment", Text, nullable=False, server_default=text("''::text")
-        )
-    )
-    story: int = Field(
-        sa_column=Column("story", Integer, nullable=False, server_default=text("4"))
-    )
-    direction: int = Field(
-        sa_column=Column("direction", Integer, nullable=False, server_default=text("4"))
-    )
-    acting: int = Field(
-        sa_column=Column("acting", Integer, nullable=False, server_default=text("4"))
-    )
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    movie_id: Optional[int] = Field(
-        default=None, sa_column=Column("movie_id", BigInteger)
-    )
-
-    movie: Optional["Movie"] = Relationship(back_populates="movie_rating")
-
-
-class MovieToActor(SQLModel, table=True):
-    __tablename__ = "movie_to_actor"
-    __table_args__ = (
-        ForeignKeyConstraint(
-            ["actor_id"],
-            ["actor.id"],
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            name="movie_to_actor_foreign_key_actor_id",
-        ),
-        ForeignKeyConstraint(
-            ["movie_id"],
-            ["movie.id"],
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            name="movie_to_actor_foreign_key_movie_id",
-        ),
-        PrimaryKeyConstraint("id", name="movie_to_actor_primary_key"),
-        UniqueConstraint(
-            "movie_id", "actor_id", name="movie_to_actor_unique_movie_id_actor_id"
-        ),
-    )
-
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    movie_id: int = Field(sa_column=Column("movie_id", BigInteger, nullable=False))
-    actor_id: int = Field(sa_column=Column("actor_id", BigInteger, nullable=False))
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-
-    actor: Optional["Actor"] = Relationship(back_populates="movie_to_actor")
-    movie: Optional["Movie"] = Relationship(back_populates="movie_to_actor")
-
-
-class MovieToGenre(SQLModel, table=True):
-    __tablename__ = "movie_to_genre"
-    __table_args__ = (
-        ForeignKeyConstraint(
-            ["genre_id"],
-            ["genre.id"],
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            name="movie_to_genre_foreign_key_genre_id",
-        ),
-        ForeignKeyConstraint(
-            ["movie_id"],
-            ["movie.id"],
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            name="movie_to_genre_foreign_key_movie_id",
-        ),
-        PrimaryKeyConstraint("id", name="movie_to_genre_primary_key"),
-        UniqueConstraint(
-            "movie_id", "genre_id", name="movie_to_genre_unique_movie_id_genre_id"
-        ),
-    )
-
-    id: int = Field(sa_column=Column("id", BigInteger, primary_key=True))
-    movie_id: int = Field(sa_column=Column("movie_id", BigInteger, nullable=False))
-    genre_id: int = Field(sa_column=Column("genre_id", BigInteger, nullable=False))
-    created_at: datetime.datetime = Field(
-        sa_column=Column(
-            "created_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-    updated_at: datetime.datetime = Field(
-        sa_column=Column(
-            "updated_at",
-            DateTime(True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-
-    genre: Optional["Genre"] = Relationship(back_populates="movie_to_genre")
-    movie: Optional["Movie"] = Relationship(back_populates="movie_to_genre")
+    credential: Optional['Credential'] = Relationship(back_populates='analysis')
+    resource: Optional['Resource'] = Relationship(back_populates='analysis')
